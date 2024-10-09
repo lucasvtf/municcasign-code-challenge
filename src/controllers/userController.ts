492467b12ea6da73813f8df7c795fe550351761b
Lucas Victor <lucassvtf@gmail.com>
2024-10-09 11:33:43 -0300

    feat: criação do userController.

---
+++ src/controllers/userController.ts
@@ -0,0 +1,69 @@
+import { Request, Response, NextFunction } from 'express';
+import  UserService  from '../services/userService';
+import { StatusCodes } from 'http-status-codes';
+
+
+class UserController {
+  private service: UserService;
+
+  constructor() {
+    this.service = new UserService();
+  }
+
+  public getAllUsers = async (_req: Request, res: Response, next: NextFunction) => {
+    try {
+      const users = await this.service.getAllUsers();
+      return res.status(StatusCodes.OK).json(users);
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  public  getUserById = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId } = req.params;
+      const user = await this.service.getUserById(+userId);
+      
+      return res.status(StatusCodes.OK).json(user);
+    } catch (error) {
+      next(error)
+    }
+  };
+
+
+  public  createUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const newUser = await this.service.createUser(req.body);
+      return res.status(StatusCodes.CREATED).json(newUser);
+    } catch (error) {
+      next(error)
+    }
+  };
+
+  public updateUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId } = req.params;
+      const updatedUser = await this.service.updateUser(+userId, req.body);
+     
+      return res.status(StatusCodes.OK).json(updatedUser);
+    } catch (error) {
+      next(error)
+    }
+  };
+
+  public deleteUser = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { userId } = req.params;
+        const deleted = await this.service.deleteUser(+userId);
+        if (deleted) {
+            return res.status(StatusCodes.NO_CONTENT).send();
+        } else {
+          return res.status(StatusCodes.NOT_FOUND).send();
+        }
+    } catch (error) {
+      next(error)
+    }
+  };
+}
+
+export default UserController
